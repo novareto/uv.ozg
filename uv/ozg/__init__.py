482d254e235b087a901eec908f7e24f7d316a694
Christian Klinger <ck@novareto.de>
2021-10-27 18:35:02 +0200

    Changes

--- uv/ozg/__init__.py
+++ uv/ozg/__init__.py
@@ -1,6 +1,14 @@
 import pathlib
+import importscan
+import importlib
+import logging
 from reiter.application.browser import TemplateLoader
 
 
 TEMPLATES = TemplateLoader(
     str((pathlib.Path(__file__).parent / "templates").resolve()), ".pt")
+
+
+def install_me(app):
+    importscan.scan(importlib.import_module(__name__))
+    logging.info(f"Import Addon {__name__}")
